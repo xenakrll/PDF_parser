8fcfc91ff5364096455ef50d11b3108f23a226df
Xena Kirillova <xenakrll@gmail.com>
2016-10-04 12:38:50 +0300

    several patterns finding among several files

--- parser.py
+++ parser.py
@@ -1,6 +1,8 @@
 import PyPDF2
 import os
 
+input_pat = input("Choose pattern to look for: ")
+patterns = input_pat.split()
 dir = '/home/xenakrll/Documents/articles/for_overview/2000-2005'
 for filename in os.listdir(dir):
     pdfFileObj = open(os.path.join(dir)+'/'+filename, 'rb')
@@ -10,11 +12,11 @@ for filename in os.listdir(dir):
         for pageNum in range(0, pdfReader.numPages):
             pageObj = pdfReader.getPage(pageNum)
             text = text + pageObj.extractText()
-        pattern = 'reentry'
-        if text.find(pattern) == -1:
-            print(filename+':  '+'Pattern not found')
-        else:
-            print(filename+':  '+'Found')
+        for pattern in patterns:
+            if text.find(pattern) == -1:
+                print(filename+':  '+'Pattern '+pattern+' not found')
+            else:
+                print(filename+':  '+pattern+' found')
     except Exception:
         print(filename+': file is unreadable')
 
