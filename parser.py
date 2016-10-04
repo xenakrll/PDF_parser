3ef72b2d2f940c2c686fa339b8bb45456e0b52fe
Xena Kirillova <xenakrll@gmail.com>
2016-10-04 11:43:18 +0300

    single pattern finding among several files

--- parser.py
+++ parser.py
@@ -1,14 +1,21 @@
 import PyPDF2
+import os
 
-pdfFileObj = open('/home/xenakrll/Documents/articles/for_overview/19/996_Detection_Algorithms_in_Implantable_Cardioverter_Defibrillators.pdf', 'rb')
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-text = ''
-for pageNum in range(0, pdfReader.numPages):
-    pageObj = pdfReader.getPage(pageNum)
-    text = text + pageObj.extractText()
-pattern = 'elephant'
-if text.find(pattern) == -1:
-    print('Failure')
-else:
-    print('Found')
+dir = '/home/xenakrll/Documents/articles/for_overview/2000-2005'
+for filename in os.listdir(dir):
+    pdfFileObj = open(os.path.join(dir)+'/'+filename, 'rb')
+    try:
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+        text = ''
+        for pageNum in range(0, pdfReader.numPages):
+            pageObj = pdfReader.getPage(pageNum)
+            text = text + pageObj.extractText()
+        pattern = 'reentry'
+        if text.find(pattern) == -1:
+            print(filename+':  '+'Pattern not found')
+        else:
+            print(filename+':  '+'Found')
+    except Exception:
+        print(filename+': file is unreadable')
+
 #print(text)
