b36e2e8b8a923a5da84d77a939062cd39ea2b780
Xena Kirillova <xenakrll@gmail.com>
2016-10-04 17:52:37 +0300

    additional check and comments added

--- parser.py
+++ parser.py
@@ -1,8 +1,8 @@
 import PyPDF2
 import os
 
-input_pat = input("Choose pattern to look for: ")
-patterns = input_pat.split()
+input_pat = input("Choose patterns to look for: ")
+patterns = input_pat.split()         #allows you look for several patterns at the same time
 dir = '/home/xenakrll/Documents/articles/for_overview'
 for filename in os.listdir(dir):
     pdfFileObj = open(os.path.join(dir)+'/'+filename, 'rb')
@@ -11,9 +11,12 @@ for filename in os.listdir(dir):
         text = ''
         for pageNum in range(0, pdfReader.numPages):
             pageObj = pdfReader.getPage(pageNum)
-            text = text + pageObj.extractText()
-        for pattern in patterns:
-            if text.find(pattern) != -1:
-                print(filename+':  '+'Pattern '+pattern+'  found')
+            text = text + pageObj.extractText()        #collects texts from each page into one string
+        if text.find('a') == -1:
+            print(filename+': search does not work')   #checks if search works in this file
+        else:
+            for pattern in patterns:
+                if text.find(pattern) != -1:
+                    print(filename+':  '+'Pattern '+pattern+'  found')
     except Exception:
         print(filename+': file is unreadable')
